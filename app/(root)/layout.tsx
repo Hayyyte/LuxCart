fd6b3d8b643e38dcb4a404ffded51fcd029bb8af
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-01-24 19:02:59 -0500

    Remake using component library.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+const Loayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            {/* Navbar */}
+            {children}
+            {/* Footer */}
+        </>
+    );
+}
+ 
+export default Loayout;
