bebf865220df36e9ae4a518b991d529ac6633b9f
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-01-29 15:17:15 -0500

    Refactored menu into components

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -8,6 +8,8 @@ import {
     SheetTitle,
   } from "@/components/ui/sheet"
 import { Button } from "../ui/button";
+import NewSheet from "./New/NewSheet";
+import GiftsSheet from "./Gifts/GiftsSheet";
 
 const Navbar = () => {
     return (
@@ -27,61 +29,8 @@ const Navbar = () => {
 
                     <ul className="flex flex-col mx-5 text-3xl gap-y-5 p-3 group">
                         {/* Gift Menu */}
-                        <Sheet>
-                            {/* Gift Menu Open */}
-                            <SheetTrigger>
-                                <li className="max-w-min group-hover:text-gray-500 text-black">
-                                    <p>
-                                        <span className="hover-underline">Gifts</span>
-                                    </p>
-                                </li>
-                            </SheetTrigger>
-                            <SheetContent side="left">
-                                <SheetTrigger className="mx-5 p-3 text-xl">
-                                    ← Back
-                                </SheetTrigger>
-                                <SheetTitle>
-                                    <Link href="/gifts">
-                                        <ul className="max-w-min group-hover:text-gray-500 text-black">
-                                            <li>
-                                                <span className="hover-underline">Gifts</span>
-                                            </li>
-                                        </ul>
-                                    </Link>
-                                </SheetTitle>
-                                <ul className="flex flex-col mx-5 text-3xl gap-y-5 p-3 group">
-                                    <li className="max-w-max group-hover:text-gray-500 text-black">
-                                        <Link href="/gifts/for-her">
-                                            <p>
-                                                <span className="hover-underline">For Her</span>
-                                            </p>
-                                        </Link>
-                                    </li>
-                                    <li className="max-w-max group-hover:text-gray-500 text-black">
-                                        <Link href="/gifts/for-him">
-                                            <p>
-                                                <span className="hover-underline">For Him</span>
-                                            </p>
-                                        </Link>
-                                    </li>
-                                    <li className="max-w-max group-hover:text-gray-500 text-black">
-                                        <Link href="/gifts/holiday">
-                                            <p>
-                                                <span className="hover-underline">Holiday</span>
-                                            </p>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </SheetContent>
-                        </Sheet>
-
-                        <li className="max-w-min group-hover:text-gray-500 text-black">
-                            <Link href="/new">
-                                    <p>
-                                        <span className="hover-underline">New</span>
-                                    </p>
-                            </Link>
-                        </li>
+                        <GiftsSheet/>
+                        <NewSheet />
                         <li className="max-w-min group-hover:text-gray-500 text-black">
                             <Link href="/women">
                                 <p>
