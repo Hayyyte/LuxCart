a1865c009b391e08d38e358585e98702531a131e
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-02-06 14:38:27 -0500

    Mobile responsive for bespoke gifts.

--- app/(root)/(home)/(navbar)/gifts/bespoke/page.tsx
+++ app/(root)/(home)/(navbar)/gifts/bespoke/page.tsx
@@ -1,20 +1,19 @@
 import Filters from "@/components/Filters";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
 import giftsData from "@/data/giftsData.json";
 
 const Bespoke = () => {
     return (
-        <main className="flex flex-col mx-64 my-12 gap-5"> 
+        <main className="flex flex-col m-6 gap-6 lg:mx-32 xl:mx-64 2xl:mx-96"> 
             <h1>Bespoke</h1>
             <div className="flex gap-4">
                 <Filters />
                 <Input type="text" placeholder="Search"/>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                 {giftsData.bespoke.map((gift) => (
-                    <Card key={gift.id} className="cursor-pointer user-select-none hover:scale-105 transition-transform duration-300">
+                    <Card key={gift.id} className="cursor-pointer min-w-[200px] user-select-none hover:scale-105 transition-transform duration-300">
                         <CardHeader>
                             <CardTitle>{gift.name}</CardTitle>
                             <CardDescription>{gift.description}</CardDescription>
