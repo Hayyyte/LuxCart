d0c55f86527df6e101a91b05645694f1706693a2
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-02-06 15:29:04 -0500

    Modified nav bar to be mobile responsive.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import '@/styles/style.css';
 import {
     Sheet,
     SheetContent,
+    SheetFooter,
     SheetTrigger,
   } from "@/components/ui/sheet";
 import NewSheet from "./New/NewSheet";
@@ -17,48 +18,51 @@ import PerfumesSheet from "./Perfumes/PerfumesSheet";
 import MakeupSheet from "./Makeup/MakeupSheet";
 import ServicesSheet from "./Services/ServicesSheet";
 import LuxSheet from "./Lux/LuxSheet";
-import { Input } from "../ui/input";
 
 const Navbar = () => {
     return (
-        <nav className="flex flex-row justify-around content-center w-full text-center border-b-2 text-black border-black-200 py-7">
-            <Sheet>
-                {/* Menu Button */}
-                <SheetTrigger>
-                    <Image src='/hamburger-menu.svg' alt='Menu' width={50} height={50} className="hover:scale-125 transition-transform duration-200"/>
-                </SheetTrigger>
-
-                <SheetContent side='left'>
-                    <SheetTrigger className="mx-5 p-3 text-xl">
-                        × Close
+        <nav className="grid grid-cols-3 w-full border-b-2 text-black border-black-200 py-7">
+            <div className="flex justify-center">
+                <Sheet>
+                    {/* Menu Button */}
+                    <SheetTrigger>
+                        <Image src='/hamburger-menu.svg' alt='Menu' width={50} height={50} className="hover:scale-125 transition-transform duration-200"/>
                     </SheetTrigger>
-                    {/* Menu Items */}
-                    <ul className="flex flex-col mx-5 text-3xl gap-y-5 p-3 group">
-                        <GiftsSheet />
-                        <NewSheet />
-                        <WomenSheet />
-                        <MenSheet />
-                        <JewelrySheet />
-                        <WatchesSheet />
-                        <PerfumesSheet />
-                        <MakeupSheet />
-                        <ServicesSheet />
-                        <LuxSheet />
-                    </ul>
-                </SheetContent>
-            </Sheet>
-
+                    <SheetContent side='left'>
+                        <SheetTrigger className="mx-5 p-3 text-xl">
+                            × Close
+                        </SheetTrigger>
+                        {/* Menu Items */}
+                        <ul className="flex flex-col mx-5 text-3xl gap-y-5 p-3 group">
+                            <GiftsSheet />
+                            <NewSheet />
+                            <WomenSheet />
+                            <MenSheet />
+                            <JewelrySheet />
+                            <WatchesSheet />
+                            <PerfumesSheet />
+                            <MakeupSheet />
+                            <ServicesSheet />
+                            <LuxSheet />
+                            <ul className="flex flex-col justify-items-end gap-y-5 mt-10 sm:hidden">
+                                <li>Account</li>
+                                <li>Cart</li>
+                            </ul>
+                        </ul>
+                    </SheetContent>
+                </Sheet>
+            </div>
+            
             {/* Logo */}
-                <Link href="/">
-                    <Image src='/lux-text.svg' alt='Logo' width={100} height={100} className='cursor-pointer text-center inline-block' />
-                </Link>
-            <div className="flex flex-row gap-x-10">
-                {/* Search */}
-                <Input type="text" placeholder="Search"/>
+            <Link href="/" className="flex justify-center">
+                <Image src='/lux-text.svg' alt='Logo' width={100} height={100} className='cursor-pointer text-center inline-block' />
+            </Link>
+
+            <div className="hidden gap-5 sm:flex sm:justify-center">
                 {/* Account */}
-                <div><p>Account</p></div>
+                <p>Account</p>
                 {/* Cart */}
-                <div><p>Cart</p></div> 
+                <p>Cart</p>
             </div>
         </nav>
     );
