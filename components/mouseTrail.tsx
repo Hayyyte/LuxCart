7365c59fbb773f501693fda083eda3cde5187375
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-02-07 16:19:24 -0500

    Fix mouseTrail for deployment.
    I tried deploying on vercel. It gave me errors because of mouseTrail.
    Finally decided to solve mouseTrail errors.

--- components/mouseTrail.tsx
+++ components/mouseTrail.tsx
@@ -4,14 +4,16 @@ import React, { useEffect } from 'react';
 
 const MouseTrail = () => {
   useEffect(() => {
-    const mouseTrail = document.querySelector(".mouse-trail");
+    const mouseTrail: HTMLElement | null = document.querySelector(".mouse-trail");
 
-    const handleMouseMove = (e) => {
-      const x = e.clientX - mouseTrail.offsetWidth / 2;
-      const y = e.clientY - mouseTrail.offsetHeight / 2;
-
-      mouseTrail.style.transform = `translate(${x}px, ${y}px)`;
-    };
+    const handleMouseMove = (e: MouseEvent) => {
+      if (mouseTrail) {
+        const x: number = e.clientX - mouseTrail.offsetWidth / 2;
+        const y: number = e.clientY - mouseTrail.offsetHeight / 2;
+      
+        mouseTrail.style.transform = `translate(${x}px, ${y}px)`;
+      }
+    }; // This is where the closing brace should be placed
 
     if (mouseTrail) {
       window.addEventListener('mousemove', handleMouseMove);
