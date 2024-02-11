523094fc1fd9088b36a6543eb57b14c043fca039
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-02-10 21:21:21 -0500

    Remove mouseTrail.
    Bad design idea. Removed it solved issues.
    It didn't look good in the first place.
    Will not add it back.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,8 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-import MouseTrail from "@/components/mouseTrail";
-
 const Loayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="overflow-x-hidden">
-            <MouseTrail></MouseTrail>
             <Navbar></Navbar>    
             {children}
             <Footer></Footer>
