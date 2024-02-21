6b8db8742160869d8ed3770001c0f516b528babe
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-02-20 23:26:56 -0500

    Add men subsections.
    Need to update items to match the categories.

---
+++ app/(root)/(home)/(navbar)/(menu)/men/bags/page.tsx
@@ -0,0 +1,23 @@
+import UrlTitle from "@/components/urlTitle";
+import SkeletonCard from "@/components/SkeletonCard"
+
+const MenBags = () => {
+    return (
+        <main className="flex flex-col m-6 gap-6 lg:mx-32 xl:mx-64 2xl:mx-96">
+            <UrlTitle />
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <SkeletonCard/>
+                <SkeletonCard/>
+                <SkeletonCard/>
+                <SkeletonCard/>
+                <SkeletonCard/>
+                <SkeletonCard/>
+                <SkeletonCard/>
+                <SkeletonCard/>
+                <SkeletonCard/>
+            </div>
+        </main>
+    );
+}
+ 
+export default MenBags;
--- app/(root)/(home)/(navbar)/(menu)/men/page.tsx
+++ app/(root)/(home)/(navbar)/(menu)/men/page.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
 import UrlTitle from "@/components/urlTitle";
 
-const Men = () => {
+const Gifts = () => {
     return (
-        <UrlTitle />
+        <main className="flex flex-col m-6 gap-6 lg:mx-32 xl:mx-64 2xl:mx-96"> 
+            <div className="flex flex-col justify-center gap-9 my-4">
+                <UrlTitle />
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    <Link href={"/men/clothing"}>Clothing</Link>
+                    <Link href={"/men/shoes"}>Shoes</Link>
+                    <Link href={"/men/bags"}>Bags</Link>
+                    <Link href={"/men/watches"}>Watches</Link>
+                    <Link href={"/men/grooming"}>Grooming</Link>
+                    <Link href={"/men/suits"}>Suits</Link>
+                </div>
+            </div>
+        </main>
     );
 }
- 
-export default Men;
+    
+export default Gifts;
