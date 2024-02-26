4e1787e94c288a80946c6d7dc9cf0b4f48950660
Hayyyte <121633621+Hayyyte@users.noreply.github.com>
2024-02-26 15:32:04 -0500

    Round divs. Add new arrivals on main.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,5 @@
 import SnakeLoop from "@/components/snakeLoop"
-// import Image from "next/image"
+import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -8,57 +8,96 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main className="flex-center mx-auto w-full max-w-screen-2xl flex-col">
       <section>
           <SnakeLoop />
-          {/* mobile carousel */}
-          <div className="h-[120vw] pt-20 mb-20 md:hidden">
-            <Carousel opts={{ align: "start", }} orientation="vertical" className="h-[100vw]" >
-              <CarouselContent className="-mt-1 h-[100vw]">
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <CarouselItem key={index} className="pt-1 transition duration-300 transform hover:-translate-y-2 py-2">
-                    <div className="p-1">
-                      <Card>
-                        <CardContent className="flex aspect-square items-center justify-center p-6 bg-slate-700 hover:bg-slate-400 rounded-xl">
-                          <span className="text-3xl font-semibold">{index + 1}</span>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious className="hover:bg-slate-600" />
-              <CarouselNext className="hover:bg-slate-600" />
-            </Carousel>
+          <div id="first_carousel">
+            {/* mobile carousel */}
+            <div className="h-[120vw] pt-20 mb-20 md:hidden">
+              <Carousel opts={{ align: "start", }} orientation="vertical" className="h-[100vw]" >
+                <CarouselContent className="-mt-1 h-[100vw]">
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index} className="pt-1 transition duration-300 transform hover:-translate-y-2 py-2">
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6 bg-slate-700 hover:bg-slate-400 rounded-xl">
+                            <span className="text-3xl font-semibold">{index + 1}</span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="hover:bg-slate-600" />
+                <CarouselNext className="hover:bg-slate-600" />
+              </Carousel>
+            </div>
+            {/* md + screen size carousel */}
+            <div className="w-full pl-14 my-2 hidden md:block bg-slate-200 rounded-3xl">
+              <Carousel opts={{ align: "start", }} className="md:w-[95%] min-w-max py-5">
+                <CarouselContent>
+                  {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index} className="basis-[33%] transition duration-300 transform hover:-translate-y-2 py-2">
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6 bg-slate-400 hover:bg-slate-700 rounded-xl">
+                            <span className="text-3xl font-semibold">{index + 1}</span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="hover:bg-slate-600" />
+                <CarouselNext className="hover:bg-slate-600" />
+              </Carousel>
+            </div>
           </div>
-          {/* md + screen size carousel */}
-          <div className="w-full pl-14 hidden md:block">
-            <Carousel
-              opts={{
-                align: "start",
-              }}
-              className="md:w-[95%] min-w-max py-5"
-            >
-              <CarouselContent>
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <CarouselItem key={index} className="basis-1/2 md:basis-[250px] lg:basis-[450px] transition duration-300 transform hover:-translate-y-2 py-2">
-                    <div className="p-1">
-                      <Card>
-                        <CardContent className="flex aspect-square items-center justify-center p-6 bg-slate-400 hover:bg-slate-700 rounded-xl">
-                          <span className="text-3xl font-semibold">{index + 1}</span>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious className="hover:bg-slate-600" />
-              <CarouselNext className="hover:bg-slate-600" />
-            </Carousel>
+          
+          <div className="grid grid-cols-1 md:grid-cols-3 w-full my-2 bg-slate-300 rounded-3xl" id="new_arrivals">
+            {/* Grid 1 */}
+            <div className="m-5 p-5 bg-slate-500 rounded-2xl">
+              <h2 className="text-3xl">New Arrivals</h2>
+              <br />
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore fugiat quo est animi consequuntur tempora sunt nihil dolor alias assumenda sed fugit voluptas quibusdam neque dolores corporis, architecto illum impedit mollitia nobis, vitae qui, molestias exercitationem. Commodi ullam, nisi consectetur similique hic saepe veniam ex, nam possimus fugiat velit officia.
+              </p>
+              <br />
+              <Button variant="outline">Shop Now</Button>
+            </div>
+            {/* Grid 2 */}
+            <div className="m-5 p-5 bg-slate-500 rounded-2xl group">
+              <div className="flex justify-center group">
+                <Image src="/../gift_images/clothing.png" alt="New Arrivals" width={750} height={100} className="block group-hover:hidden" />
+                <Image src="/../gift_images/dresses.png" alt="New Arrivals" width={750} height={250} className="hidden group-hover:block" />
+              </div>
+              <div className="flex justify-between py-2">
+                <div>
+                  <h3>Clothing Name</h3>
+                  <p>Category</p>
+                </div>
+                <Button variant="outline">Add to Cart</Button>
+              </div>
+            </div>
+            {/* Grid 3 */}
+            <div className="m-5 p-5 bg-slate-500 rounded-2xl">
+              <div className="flex justify-center group">
+                <Image src="/../gift_images/dresses.png" alt="New Arrivals" width={750} height={250} className="block group-hover:hidden" />
+                <Image src="/../gift_images/clothing.png" alt="New Arrivals" width={750} height={100} className="hidden group-hover:block" />
+              </div>
+              <div className="flex justify-between py-2">
+                <div>
+                  <h3>Dress Name</h3>
+                  <p>Category</p>
+                </div>
+                <Button variant="outline">Add to Cart</Button>
+              </div>
+            </div>
           </div>
+
       </section>
     </main>
   )
--- components/snakeLoop.tsx
+++ components/snakeLoop.tsx
@@ -1,6 +1,6 @@
 const SnakeLoop = () => {
     return (
-        <div style={{position: 'relative', width: '100%', maxWidth: '100vw', overflow: 'hidden',}}>
+        <div className="my-2 rounded-3xl" style={{position: 'relative', width: '100%', maxWidth: '100vw', overflow: 'hidden',}}>
             <video autoPlay loop muted playsInline style={{ width: '100%', height: 'auto' }}>
                 <source src="/snake-loop.mp4" type="video/mp4" />
             </video>
